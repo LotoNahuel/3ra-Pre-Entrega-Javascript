1d7fd976255a302a60114c6a9d477ea2126acbab
NAHUEL LOTO <loto_nahuel@hotmail.com>
2023-12-16 00:28:20 -0300

    Falta

--- static/js/posteos.js
+++ static/js/posteos.js
@@ -1,3 +1,10 @@
-const key_post = JSON.parse(localStorage.getItem("publicacion - 14"))
+const key_post = JSON.parse(localStorage.getItem("publicacion - 19"))
 const photo = document.getElementById("photo")
+const titulo = document.getElementById("title_post")
+const descripcion = document.getElementById("text_post")
+const name_user = document.getElementById("name_user")
+
+name_user.innerHTML = `${key_post[0].usuario}`
 photo.innerHTML =`<img src="${key_post[0].photo}" class="img-fluid" alt="post"></img>`;
+titulo.innerHTML = `${key_post[0].name_post}`
+descripcion.innerHTML = `${key_post[0].description}`
